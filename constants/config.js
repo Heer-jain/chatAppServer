84820f7ccbfabc61e6b185ae0ca54f025dd89e34
Heer-jain <heerjain85518@gmail.com>
2025-07-16 12:00:20 +0530

    Finally

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOption = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://chat-app-client-zeta-sepia.vercel.app/"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://chat-app-client-zeta-sepia.vercel.app/", process.env.CLIENT_URL],
     credentials: true
 }
 
